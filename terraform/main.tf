93f4d057c1e1fefd6f1b41dbb07d154c311cc898
s1mba <danilmpower@gmail.com>
2023-11-12 19:31:03 +0300

    Working version v1

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "yandex" {
   folder_id                = local.folder_id
   zone                     = "ru-central1-a"
 }
-
+# ----------------------------------------------
 resource "yandex_vpc_network" "foo" {}
 
 resource "yandex_vpc_subnet" "foo" {
@@ -21,37 +21,69 @@ resource "yandex_vpc_subnet" "foo" {
   v4_cidr_blocks = ["10.5.0.0/24"]
 }
 
-resource "yandex_container_registry" "registry1" {
-  name = "registry1"
+locals {
+  folder_id = "b1gt4qt8vt2ld07rqj9u"
+  service-accounts = toset([
+    "catgpt-service-account",
+    "catgpt-ig-sa"
+  ])
+  catgpt-service-account-roles = toset([
+    "container-registry.images.puller",
+    "monitoring.editor"
+  ])
+  catgpt-ig-roles = toset([
+    "compute.editor",
+    "iam.serviceAccounts.user",
+    "load-balancer.admin",
+    "vpc.publicAdmin",
+    "vpc.user"
+  ])
 }
 
-locals {
-  folder_id = "<INSERT YOUR FOLDER ID>"
-  service-accounts = toset([
-    "catgpt-sa",
-  ])
-  catgpt-sa-roles = toset([
-    "container-registry.images.puller",
-    "monitoring.editor",
-  ])
-}
-resource "yandex_iam_service_account" "service-accounts" {
+resource "yandex_iam_service_account" "service-accounts" { # create Service Accounts
   for_each = local.service-accounts
-  name     = each.key
+  name     = "${local.folder_id}-${each.key}"
 }
-resource "yandex_resourcemanager_folder_iam_member" "catgpt-roles" {
-  for_each  = local.catgpt-sa-roles
+
+resource "yandex_resourcemanager_folder_iam_member" "catgpt-service-account-roles" { # Adding IAM roles to push in container registry
+  for_each  = local.catgpt-service-account-roles
   folder_id = local.folder_id
-  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-sa"].id}"
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-service-account"].id}"
   role      = each.key
 }
 
-data "yandex_compute_image" "coi" {
+resource "yandex_resourcemanager_folder_iam_member" "catgpt-ig-roles" { # Adding IAM roles to push in container registry
+  for_each  = local.catgpt-ig-roles
+  folder_id = local.folder_id
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-ig-sa"].id}"
+  role      = each.key
+}
+
+data "yandex_compute_image" "coi" { # Latest Container Optimized Image https://cloud.yandex.ru/docs/cos/concepts/
   family = "container-optimized-image"
 }
-resource "yandex_compute_instance" "catgpt-1" {
-    platform_id        = "standard-v2"
-    service_account_id = yandex_iam_service_account.service-accounts["catgpt-sa"].id
+
+resource "yandex_compute_instance_group" "catgpt" {
+  depends_on = [ yandex_resourcemanager_folder_iam_member.catgpt-ig-roles ]
+  name = "catgpt"
+  service_account_id = yandex_iam_service_account.service-accounts["catgpt-ig-sa"].id
+  allocation_policy {
+    zones = [ "ru-central1-a" ]
+  }
+  deploy_policy {
+    max_unavailable = 1
+    max_creating    = 2
+    max_expansion   = 2
+    max_deleting    = 2
+  }
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
+  }
+  instance_template {
+    platform_id = "standard-v2"
+    service_account_id = yandex_iam_service_account.service-accounts["catgpt-service-account"].id
     resources {
       cores         = 2
       memory        = 1
@@ -61,8 +93,9 @@ resource "yandex_compute_instance" "catgpt-1" {
       preemptible = true
     }
     network_interface {
-      subnet_id = "${yandex_vpc_subnet.foo.id}"
-      nat = true
+      network_id = yandex_vpc_network.foo.id
+      subnet_ids = ["${yandex_vpc_subnet.foo.id}"]
+      nat        = true
     }
     boot_disk {
       initialize_params {
@@ -72,9 +105,36 @@ resource "yandex_compute_instance" "catgpt-1" {
       }
     }
     metadata = {
-      docker-compose = file("${path.module}/docker-compose.yaml")
-      ssh-keys  = "ubuntu:${file("~/.ssh/devops_training.pub")}"
+      docker-compose = templatefile("${path.module}/docker-compose.yaml", {
+        folder_id = "${local.folder_id}",
+      })
+      ssh-keys  = "ubuntu:${file("~/.ssh/yandex.pub")}"
+      user-data = file("${path.module}/cloud-config.yaml")
     }
+  }
+  load_balancer {
+    target_group_name = "catgpt"
+  }
 }
 
-
+resource "yandex_lb_network_load_balancer" "lb-catgpt" {
+  name = "catgpt"
+  listener {
+    name = "cat-listener"
+    port = 80
+    target_port = 8080
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+  attached_target_group {
+    target_group_id = yandex_compute_instance_group.catgpt.load_balancer[0].target_group_id
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 8080
+        path = "/ping"
+      }
+    }
+  }
+}
